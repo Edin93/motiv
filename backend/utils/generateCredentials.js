ac51b52edc93545475a847c81c2d7e9046f8af74
Mxn-ptr <maxence0@hotmail.fr>
2023-08-10 11:04:23 +0200

    Add function to generate a secure temporary password

---
+++ backend/utils/generateCredentials.js
@@ -0,0 +1,38 @@
+// Generate a code with 4 digits
+module.exports.generateConfirmationEmailCode = () => {
+  return Math.floor(Math.random() * 9000) + 1000;
+}
+
+// Generate a password with at least one uppercase character, one lowercase character, one number and one symbol
+module.exports.generateNewPassword = () => {
+  const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numbers = '0123456789';
+  const symbols = ['&','!','*','$','?','-'];
+
+  const allCharacters = lowercaseLetters + uppercaseLetters + numbers;
+
+  let password = '';
+
+  password += lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)];
+  password += uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+
+  const randomSymbolIndex1 = Math.floor(Math.random() * symbols.length);
+  let randomSymbolIndex2 = Math.floor(Math.random() * symbols.length);
+  while (randomSymbolIndex2 === randomSymbolIndex1) {
+    randomSymbolIndex2 = Math.floor(Math.random() * symbols.length);
+  }
+  password += symbols[randomSymbolIndex1];
+  password += symbols[randomSymbolIndex2];
+
+  for (let i = password.length; i < 10; i++) {
+    const randomIndex = Math.floor(Math.random() * allCharacters.length);
+    password += allCharacters[randomIndex];
+  }
+
+  password = password.split('').sort(() => Math.random() - 0.5).join('');
+
+  console.log(password);
+  return password;
+}
--- backend/utils/generateId.js
+++
@@ -1,8 +0,0 @@
-// Generate a code with 4 digits
-module.exports.generateConfirmationEmailCode = () => {
-  return Math.floor(Math.random() * 9000) + 1000;
-}
-
-module.exports.generateNewPassword = () => {
-  return "test";
-}
