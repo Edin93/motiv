85b9a3534436f60d3b2000ab16866c1f770a7a4d
Mxn-ptr <maxence0@hotmail.fr>
2023-09-04 20:35:54 +0200

    Add first part of events

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const activityRouter = require('./routes/ActivityRouter');
 const userRouter = require('./routes/UserRouter');
+const eventRouter = require('./routes/EventRouter');
 const cors = require('cors');
+const { deleteCompletedEvents } = require("./utils/controllerFunctions");
 
 const PORT = process.env.PORT || 5050;
 
@@ -26,7 +28,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/activities", activityRouter);
+app.use("/api/events", eventRouter);
 app.use("/api/users", userRouter);
+
+deleteCompletedEvents();
 
 app.listen(
     PORT,
